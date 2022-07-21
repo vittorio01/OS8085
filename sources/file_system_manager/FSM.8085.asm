0e40d430e9e885e05046dab08f50b52bd429ff21
vittorio01 <vittoriopolci@live.com>
2022-07-21 10:00:09 +0200

    aggiunta un'implementazione parziale della formattazione

--- sources/file_system_manager/FSM.8085.asm
+++ sources/file_system_manager/FSM.8085.asm
@@ -28,16 +28,22 @@
 ;per comodità viene presa in considerazione un insieme di settori del disco (una pagina) composta da  più settori.
 ;----- Struttura dei files -----
 ;Un file è dotato di intestazine e corpo, che vengono separati e memorizzati in due diverse zone della memoria. L'intestazione è formata da:
-;-  un byte per identificare il tipo di file 
-;-  18 bytes per memorizzare nome ed estenzione del file, separate da un punto (ad esempio file.exe) 
-;-  2 bytes per indicare il numero di blocchi occupati dal file
-;-  2 bytes che mantengono l'indirizzo della prima pagina dei dati
+;-  tipo        ->  un byte che identifica la tipologia di file. I bit assumono valori diversi secondo le seguenti caratteristiche:
+;               * bit 8 -> bit di valdità settato sempre per indicare che in quela posizione +è presente un intestazione
+;               * bit 7 -> bit che indica se il file è di sistema o no
+;               * bit 6 -> bit che identifica se il file è eseguibile o no
+;               * bit 5 -> bit che indica se il file è nascosto 
+;               
+;-  nome        -> 20 bytes per memorizzare nome ed estenzione del file, separate da un punto (ad esempio file.exe) 
+;-  data        -> 7 bytes che memorizzano la data di creazione del file (codificata in BCD a partire dall'anno)
+;-  dimensione  -> 2 bytes per indicare il numero di blocchi occupati dal file
+;-  dati        -> 2 bytes che mantengono l'indirizzo della prima pagina dei dati
 
 ;-------------------------------------------------------------------------------------------
-;- tipo - nome ed estenzione - dimensione (in pagine) - puntatore alla prima pagina indici -
+;- tipo - nome ed estenzione - dimensione (in pagine) - puntatore alla prima pagina dati -
 ;-------------------------------------------------------------------------------------------
 
-;L'intestazione di un file è quindi di dimensone fissa prestabilita (32 bytes)
+;L'intestazione di un file è quindi di dimensione fissa prestabilita (32 bytes)
 
 ;----- Tabella di allocazione -----
 ;Il corpo dei files viene spezzato in pagine e salvato in posizioni non per forza contigue del disco. Un file di dimensione N se deve essere salvato in un disco 
@@ -105,9 +111,10 @@
 ;       * numero di tracce per testina          (2 bytes)
 ;       * numero di settori per traccia         (1 byte)
 ;       * numero di bytes per settore           (1 byte)
-;       * numero di pagine disponibii           (2 bytes)
-;       * puntatore al primo settore della fat  (2 bytes)
-;       * numero di settori dedicate al sistema (4 bytes)
+;       * numero di settori per pagina          (1 byte)
+;       * numero di pagine dedicate alla fat    (1 byte)
+;       * numero di pagine dedicate ai dati     (2 bytes)
+;       * puntatore al primo settore della fat  (4 bytes)
 ;       Il resto del settore contiene le istruzioni per l'avvio del sistema operativo (se presenti)
 
 ;-  Il sistema operativo occupa una certa zona riservata del disco, oltre alla parte rimanente del settore di avvio, e può avere una dimensione massima di 64KB (dato che deve essere caricao in memoria)
@@ -117,4 +124,345 @@
 ;----------------------------------------------------------------------
 ;- settore di avvio - sistema operativo (opzionale) - FAT - Zona dati -
 ;----------------------------------------------------------------------
+
+
+fsm_selected_disk                       .equ $0060
+fsm_selected_disk_head_number           .equ $0061
+fsm_selected_disk_tph_number            .equ $0062 
+fsm_selected_disk_spt_number            .equ $0064 
+fsm_selected_disk_bps_number            .equ $0065
+fsm_selected_disk_sectors_number        .equ $0066
+fsm_selected_disk_spp_number            .equ $006A
+fsm_selected_disk_data_first_sector     .equ $006B
+fsm_page_buffer_segment_id              .equ $006D
+fsm_page_buffer_segment_address         .equ $006E
+fsm_selected_disk_data_page_number      .equ $0070
+fsm_selected_disk_fat_page_number       .equ $0072
+fsm_selected_disk_loaded_page           .equ $0073
+fsm_selected_disk_loaded_page_flags     .equ $0075
+
+
+fsm_coded_page_dimension            .equ 16
+fsm_uncoded_page_dimension          .equ 2048
+
+fsm_format_marker_lenght            .equ 6 
+fms_disk_name_max_lenght            .equ 16 
+
+fsm_page_loaded_mask            .equ %10000000
+fsm_page_type_mask              .equ %01000000 ;(0 per tipo fat, 1 per tipo data)
+
+fsm_mass_memory_sector_not_found    .equ $20
+fsm_operation_ok                    .equ $ff
+
+fsm_format_marker   .text "SFS1.0"
+                    .b $00
+
+
+fsm_functions:  .org FSM 
+                jmp fsm_init 
+                jmp fsm_disk_format 
+                ;jmp fsm_disk_wipe 
+                ;jmp fsm_disk_mount 
+
+;fsm_init inizializza la fsm 
+
+fsm_init:   xra a 
+            sta fsm_selected_disk
+            sta fsm_selected_disk_loaded_page
+            sta fsm_selected_disk_loaded_page+1
+            sta fsm_selected_disk_loaded_page_flags
+            ret 
+
+;fsm_disk_format formatta il disco e prepara il file system di base 
+; A  -> disco da formattare 
+; DE -> nome del disco (puntatore a una stringa)
+; HL -> dimensione della sezione riservata al sistema (in bytes)
+fsm_disk_format:        
+                        push b 
+                        push d 
+                        push h 
+                        push psw 
+                        ;call bios_mass_memory_format_device
+                        ;cpi bios_operation_ok 
+                        pop psw 
+                        ;jz fsm_disk_external_generated_error
+                        sta fsm_selected_disk
+                        call bios_mass_memory_select_drive
+                        ora a 
+                        jz fsm_disk_external_generated_error
+                        sta fsm_selected_disk_bps_number
+                        mov a,b 
+                        sta fsm_selected_disk_spt_number
+                        mov a,c 
+                        sta fsm_selected_disk_head_number
+                        xchg 
+                        shld fsm_selected_disk_tph_number
+                        call unsigned_multiply_byte
+                        xchg 
+                        call unsigned_multiply_word 
+                        xchg 
+                        shld fsm_selected_disk_sectors_number
+                        xchg 
+                        mov l,c 
+                        mov h,b 
+                        shld fsm_selected_disk_sectors_number+2
+                        lda fsm_selected_disk_bps_number
+                        mov c,a 
+                        mvi b,fsm_coded_page_dimension
+                        call unsigned_divide_byte 
+                        mov a,b 
+                        sta fsm_selected_disk_spp_number 
+                        lda fsm_selected_disk_bps_number
+                        mov b,a 
+                        mvi c,128
+                        call unsigned_multiply_byte
+                        mov e,c 
+                        mov d,b 
+                        pop d 
+                        push d 
+                        call unsigned_divide_word
+                        mov a,e 
+                        ora d 
+                        jnz fsm_disk_format_jump1
+                        inx b 
+fsm_disk_format_jump1:  inx b 
+                        mov l,c 
+                        mov h,b 
+                        shld fsm_selected_disk_data_first_sector
+                        xra a 
+                        call bios_mass_memory_select_head
+                        cpi bios_operation_ok
+                        jnz fsm_disk_external_generated_error
+                        xra a 
+                        call bios_mass_memory_select_sector
+                        cpi bios_operation_ok
+                        jnz fsm_disk_external_generated_error
+                        lxi h,0 
+                        call bios_mass_memory_select_track
+                        cpi bios_operation_ok
+                        jnz fsm_disk_external_generated_error
+                        mvi b,fsm_coded_page_dimension 
+                        mvi c,128 
+                        call unsigned_multiply_byte
+                        mov l,c 
+                        mov h,b 
+                        call mms_create_low_memory_system_data_segment
+                        ora a 
+                        jz fsm_disk_external_generated_error
+                        sta fsm_page_buffer_segment_id 
+                        call mms_read_selected_system_segment_data_address
+                        shld fsm_page_buffer_segment_address
+                        call fsm_clear_mms_segment
+                        mvi m,$c9 
+                        inx h 
+                        inx h 
+                        inx h 
+                        lxi d,fsm_format_marker
+                        mvi a,fsm_format_marker_lenght 
+                        call string_ncopy
+                        mvi a,fsm_format_marker_lenght 
+                        add l 
+                        mov l,a 
+                        mov a,h 
+                        aci 0 
+                        mov h,a 
+                        inx sp 
+                        inx sp 
+                        pop d 
+                        push d 
+                        dcx sp 
+                        dcx sp 
+                        mvi a,fms_disk_name_max_lenght
+                        call string_ncopy
+                        mvi a,fms_disk_name_max_lenght
+                        add l 
+                        mov l,a 
+                        mov a,h 
+                        aci 0 
+                        mov h,a 
+                        inx h
+                        mvi m,0 
+                        inx h 
+                        lda fsm_selected_disk_head_number
+                        mov m,a 
+                        inx h 
+                        lda fsm_selected_disk_tph_number
+                        mov m,a 
+                        inx h 
+                        lda fsm_selected_disk_tph_number+1 
+                        mov m,a 
+                        inx h 
+                        lda fsm_selected_disk_spt_number
+                        mov m,a 
+                        inx h 
+                        lda fsm_selected_disk_spp_number
+                        mov m,a 
+                        inx h 
+                        lxi d,0 
+                        push d 
+                        lxi d,fsm_uncoded_page_dimension 
+                        push d 
+                        lda fsm_selected_disk_data_first_sector
+                        mov e,a 
+                        lda fsm_selected_disk_data_first_sector+1
+                        mov d,a 
+                        lda fsm_selected_disk_sectors_number
+                        sub e 
+                        mov e,a 
+                        lda fsm_selected_disk_sectors_number+1
+                        sub d 
+                        mov d,a 
+                        lda fsm_selected_disk_sectors_number+2
+                        sbi 0 
+                        mov c,a 
+                        lda fsm_selected_disk_sectors_number+3
+                        sbi 0 
+                        mov b,a 
+                        push b 
+                        push d 
+                        call unsigned_divide_long
+                        pop d 
+                        pop d 
+                        pop d 
+                        pop b
+                        mov a,e 
+                        sta fsm_selected_disk_data_page_number
+                        mov a,d 
+                        sta fsm_selected_disk_data_page_number+1
+                        push b  
+                        lxi b,fsm_uncoded_page_dimension+2 
+                        push b 
+                        lxi b,2
+                        call unsigned_multiply_word 
+                        push d 
+                        push b 
+                        call unsigned_divide_long
+                        pop d 
+                        pop b 
+                        mov a,e 
+                        ora d 
+                        ora c 
+                        ora b 
+                        jz fsm_disk_format_jump2 
+                        mvi a,1
+fsm_disk_format_jump2:  pop d 
+                        pop b 
+                        mov a,e 
+                        add a 
+                        mov e,a 
+                        sta fsm_selected_disk_fat_page_number
+                        mov m,a 
+                        inx h 
+                        lda fsm_selected_disk_data_page_number
+                        sub e 
+                        mov m,a 
+                        inx h 
+                        sta fsm_selected_disk_data_page_number
+                        lda fsm_selected_disk_data_page_number+1
+                        sbi 0 
+                        mov m,a 
+                        inx h 
+                        sta fsm_selected_disk_data_page_number+1
+                        inx h 
+                        lda fsm_selected_disk_data_first_sector
+                        mov m,a 
+                        inx h 
+                        lda fsm_selected_disk_data_first_sector+1  
+                        mov m,a 
+                        inx h 
+                        mvi m,0 
+                        inx h 
+                        mvi m,0 
+                        lhld fsm_page_buffer_segment_address
+                        call bios_mass_memory_write_sector
+                        cpi bios_operation_ok
+                        jnz fsm_disk_external_generated_error
+
+                        mvi a,fsm_operation_ok 
+                        pop h 
+                        pop d 
+                        pop b 
+                        ret 
+
+fsm_disk_external_generated_error:      pop h 
+                                        pop d 
+                                        pop b 
+                                        ret 
+
+
+
+fsm_clear_mms_segment:      push h 
+                            push d 
+                            lhld fsm_page_buffer_segment_address
+                            lxi d,fsm_uncoded_page_dimension
+fsm_clear_mms_segment_loop: mvi m,0 
+                            inx h 
+                            dcx d 
+                            mov a,d 
+                            ora e 
+                            jnz fsm_clear_mms_segment_loop
+                            pop d 
+                            pop h 
+                            ret 
+
+
+;fsm_seek_mass_memory_sector posiziona la testina nel settore specificato
+;BCDE -> posizione in settori
+;A <- esito dell'operazione
+fsm_seek_disk_sector:                       push b 
+                                            push d 
+                                            push h 
+                                            lda fsm_selected_disk_sectors_number
+                                            sub e 
+                                            lda fsm_selected_disk_sectors_number+1
+                                            sbb d 
+                                            lda fsm_selected_disk_sectors_number+2
+                                            sbb c 
+                                            lda fsm_selected_disk_sectors_number+2
+                                            sbb b 
+                                            jnc fsm_seek_disk_sector_not_overflow
+                                            mvi a,fsm_mass_memory_sector_not_found
+                                            jmp fsm_seek_disk_sector_error
+fsm_seek_disk_sector_not_overflow:          lxi h,0 
+                                            push h 
+                                            lda fsm_selected_disk_spt_number
+                                            mov l,a
+                                            push h 
+                                            push b 
+                                            push d  
+                                            call unsigned_divide_long 
+                                            pop h 
+                                            inx sp
+                                            inx sp 
+                                            pop d 
+                                            pop b 
+                                            mov a,l 
+                                            call bios_mass_memory_select_sector
+                                            cpi bios_operation_ok
+                                            jnz fsm_seek_disk_sector_error
+                                            lhld fsm_selected_disk_tph_number
+                                            push h 
+                                            lxi h,0 
+                                            push h 
+                                            push b 
+                                            push d 
+                                            call unsigned_divide_long 
+                                            pop h 
+                                            inx sp 
+                                            inx sp 
+                                            pop b 
+                                            inx sp 
+                                            inx sp 
+                                            call bios_mass_memory_select_track
+                                            cpi bios_operation_ok
+                                            jnz fsm_seek_disk_sector_error
+                                            mov a,c 
+                                            call bios_mass_memory_select_head
+                                            cpi bios_operation_ok
+                                            jnz fsm_seek_disk_sector_error
+                                            mvi a,fsm_operation_ok
+fsm_seek_disk_sector_error:                 pop h 
+                                            pop d 
+                                            pop b 
+                                            ret 
 
