e86bdafc3ce7d3d7c786a6d863990f4270a54fcc
vittorio01 <vittoriopolci@live.com>
2022-08-03 19:05:46 +0200

    modifiche generali

--- sources/console_processor/CPS.8085.asm
+++ sources/console_processor/CPS.8085.asm
@@ -2,26 +2,15 @@ cps_functions:      .org CPS
                     jmp system_boot
 
 system_boot:    lxi sp,$00bf
-                lxi b,0 
-                push b 
-                lxi b,$A000 
-                push b 
-                lxi b,0 
-                push b 
-                lxi b,$800
-                push b 
-                call unsigned_divide_long
-                hlt 
-
-
                 call bios_warm_boot
                 call mms_low_memory_initialize
                 call fsm_init 
                 mvi a,'A'
-                lxi h,0 
+                lxi h,0
                 call fsm_disk_format
                 hlt
 
 disk_name:  .text "DISCO DI PROVA"
             .b 0
+
 CPS_level_end:
