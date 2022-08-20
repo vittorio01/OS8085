48db09cc9f6790a1f6ad4a8bc9cc748cdf9dc344
vittorio01 <vittoriopolci@live.com>
2022-08-20 18:01:16 +0200

    modifiche generali

--- sources/console_processor/CPS.8085.asm
+++ sources/console_processor/CPS.8085.asm
@@ -7,14 +7,23 @@ system_boot:    lxi sp,stack_memory_start
                 call fsm_init
                 mvi a,$41
                 call fsm_select_disk
-                lxi h,0
-                mvi a,2
-                call fsm_append_pages
+                lxi b,file_name
+                lxi d,extension_name
+                call fsm_select_file_header
+                lxi b,file_name2
+                lxi d,extension_name2
+                call fsm_set_selected_file_header_name_and_extension
                 hlt
 
 file_name:  .text "HEADER5"
             .b 0
 
 extension_name: .text "EXT5"
+                .b 0
+
+file_name2:  .text "HEADER6"
+            .b 0
+
+extension_name2: .text "EXT6"
                 .b 0
 CPS_level_end:
