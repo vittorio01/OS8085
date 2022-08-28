22a36a373a34efdb18e5753b0c3c2abed8164e7d
vittorio01 <vittoriopolci@live.com>
2022-08-28 19:05:02 +0200

    modifiche generali

--- sources/console_processor/CPS.8085.asm
+++ sources/console_processor/CPS.8085.asm
@@ -7,17 +7,16 @@ system_boot:    lxi sp,stack_memory_start
                 call fsm_init
                 mvi a,$41
                 call fsm_select_disk
-                jmp system_boot 
                 lxi b,file_name
                 lxi d,extension_name
                 call fsm_select_file_header
-                call fsm_increment_file_header_scan_pointer
+                call fsm_delete_selected_file_header
                 hlt
 
-file_name:  .text "HEADER4"
+file_name:  .text "HEADERo"
             .b 0
 
-extension_name: .text "EXT4"
+extension_name: .text "EXTo"
                 .b 0
 
 CPS_level_end:
