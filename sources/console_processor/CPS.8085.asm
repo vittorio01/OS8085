424f207801efd1d1d724f5ebece48eb45ca40300
vittorio01 <vittoriopolci@live.com>
2022-08-29 12:40:36 +0200

    modifiche generali

--- sources/console_processor/CPS.8085.asm
+++ sources/console_processor/CPS.8085.asm
@@ -7,10 +7,7 @@ system_boot:    lxi sp,stack_memory_start
                 call fsm_init
                 mvi a,$41
                 call fsm_select_disk
-                lxi b,file_name
-                lxi d,extension_name
-                call fsm_select_file_header
-                call fsm_delete_selected_file_header
+                call fsm_disk_get_name
                 hlt
 
 file_name:  .text "HEADERo"
