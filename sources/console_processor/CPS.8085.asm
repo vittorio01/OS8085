a9624c4418f4f614036f830c3ca2427bad2ee0fc
vittorio01 <vittoriopolci@live.com>
2022-09-24 22:19:38 +0200

    modifiche generali

--- sources/console_processor/CPS.8085.asm
+++ sources/console_processor/CPS.8085.asm
@@ -2,26 +2,27 @@ cps_functions:      .org CPS
                     jmp system_boot
 
 system_boot:    lxi sp,stack_memory_start
-                ;call bios_warm_boot
+                call bios_warm_boot
                 call mms_low_memory_initialize
-                mvi a,%10100000
-                lxi h,64 
-                call mms_create_low_memory_data_segment
+                mvi a,bios_mass_memory_rom_id 
+                call bios_mass_memory_select_drive
+                mvi a,0 
+                call bios_mass_memory_select_head
+                mvi a,0 
+                call bios_mass_memory_select_sector
+                lxi h,0 
+                call bios_mass_memory_select_track
                 mvi a,%11000000
-                lxi h,128
+                lxi h,512
                 call mms_create_low_memory_data_segment
-                mvi a,%10100000
-                lxi h,64 
-                call mms_create_low_memory_data_segment
-                mvi a,%11000000
-                lxi h,128
-                call mms_create_low_memory_data_segment
-                mvi a,%10100000
-                lxi h,256
-                call mms_create_low_memory_data_segment
-                call mms_delete_all_temporary_segments
-                mvi a,$33
-                lhld mms_data_low_pointer
+                lxi h,0
+                call mms_mass_memory_read_sector
+                lxi h,1024 
+                call mms_load_low_memory_program
+                lxi b,256 
+                lxi d,0 
+                lxi h,0
+                call mms_program_bytes_read
                 hlt 
                 ;call fsm_init
                 ;mvi a,$41
